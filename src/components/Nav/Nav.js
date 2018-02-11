d97e9af30794dfa39f2116487336c84a54c74406
viet <vietjul@gmail.com>
2018-02-10 20:15:44 -0500

    remove font-awesome

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,7 +3,7 @@ import './Nav.css';
 
 const Nav = props => (
     <nav className="nav bg-primary">
-        <a className="nav-link text-white" href="/"><b><i className="fas fa-shopping-cart logo"></i> Cart.ly</b></a>
+        <a className="nav-link text-white" href="/"><b><span role="img" aria-label="cart">🛒</span>Cart.ly</b></a>
         <a className="nav-link text-white" href="#" onClick={props.showShop}>Shop</a>
         <a className="nav-link text-white ml-auto" href="#" onClick={props.showCart}>Your Cart <span className="item-count text-primary">{props.itemCount}</span></a>
     </nav>
